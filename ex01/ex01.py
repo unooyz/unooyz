53443b006d320fd73afe124ca00fd16e559594a8
Kyoung Mook Jung <unooyz@naver.com>
2016-03-15 12:56:39 +0900

    ex01.py  내용추가

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,1 +1,8 @@
 print "hello world"
+print "hello again"
+print "I like typing this"
+print " this is fun."
+print 'Yay! printing'
+print " I'd much rather you 'not'."
+print ' I "said" do not touch this.'
+
