9b7cec5e738968706f19632677e21dcaf33a15f7
CAD Client <CAD Client>
2016-03-29 11:42:27 +0900

    centimeter to inches

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,13 +1,16 @@
-name = 'Jung kyoung mook'
-age = 23 # not a lie, but i dnt't believe it lol, // if 23 has "" (look like "23") this is a word (don't have '' , the 23 is a number)
-my_height = 182.5 #cm
+my_name = 'Jung kyoung mook'
+my_age = 23 # not a lie, but i dnt't believe it lol, // if 23 has "" (look like "23") this is a word (don't have '' , the 23 is a number)
+my_height_cm = 182.5 #cm
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
 my_weight = 75 #kg
 my_eyes = 'black'
 my_teeth = 'white'
 my_hair=  'black'
 
-print "Let's talk about %s." % name
-print "He's %d centimeter tall" % my_height
+print "Let's talk about %s." % my_name
+print "He's %g centimeter tall" % my_height_cm
+print "He's %g inches tall" % my_height_inch
 print "He's %d kilogram heavy." % my_weight
 print "Actually tha's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
@@ -15,4 +18,4 @@ print "His teeth are uaually %s depending on the coffee." % my_teeth
 
 # this live is tricky, try to get it exactly right
 print "IF I add %d, %d, and %d I get %d." % (
-       age, my_height, my_weight, age + my_height + my_weight)
+       my_age, my_height_cm, my_weight, my_age + my_height_cm + my_weight)
