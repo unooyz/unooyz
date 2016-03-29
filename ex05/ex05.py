77b6b5d1ea58d0b192250070d61cb0dc118f763f
CAD Client <CAD Client>
2016-03-29 11:14:46 +0900

    Compare refactor (명령어를 한번에 바꾸는 기능, 그냥 바꾸면 밑에는 안바뀜)

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,12 +1,12 @@
-my_name = 'Jung kyoung mook'
-my_age = 23 # not a lie, but i dnt't believe it lol, // if 23 has "" (look like "23") this is a word (don't have '' , the 23 is a number)
+name = 'Jung kyoung mook'
+age = 23 # not a lie, but i dnt't believe it lol, // if 23 has "" (look like "23") this is a word (don't have '' , the 23 is a number)
 my_height = 182.5 #cm
 my_weight = 75 #kg
 my_eyes = 'black'
 my_teeth = 'white'
 my_hair=  'black'
 
-print "Let's talk about %s." % my_name
+print "Let's talk about %s." % name
 print "He's %d centimeter tall" % my_height
 print "He's %d kilogram heavy." % my_weight
 print "Actually tha's not too heavy."
@@ -15,4 +15,4 @@ print "His teeth are uaually %s depending on the coffee." % my_teeth
 
 # this live is tricky, try to get it exactly right
 print "IF I add %d, %d, and %d I get %d." % (
-       my_age, my_height, my_weight, my_age + my_height + my_weight)
+       age, my_height, my_weight, age + my_height + my_weight)
