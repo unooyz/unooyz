94aeab61117915cab6d87288fbba97777532ed6d
CAD Client <CAD Client>
2016-04-05 10:11:07 +0900

    Study drill 7

--- ex07/ex07.py
+++ ex07/ex07.py
@@ -1,23 +1,26 @@
-print "May had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that mary went."
-print "." * 10 # what'd that do?
+print "May had a little lamb." # Do print "May had a little lamb."
+print "Its fleece was white as %s." % 'snow' # Do print "Its fleece was white as %s." and %s is snow
+print "And everywhere that mary went." # Do print "And everywhere that mary went."
+print "." * 10 # what'd that do? =>  -> this is that Word "."  print 10 time
 
-end1 = "c"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "c" # You remember end1 = Word "c"
+end2 = "h" # Same a line6
+end3 = "e" # Same a line6
+end4 = "e" # Same a line6
+end5 = "s" # Same a line6
+end6 = "e" # Same a line6
+end7 = "B" # Same a line6
+end8 = "u" # Same a line6
+end9 = "r" # Same a line6
+end10 = "g" # Same a line6
+end11 = "e" # Same a line6
+end12 = "r" # Same a line6
 
 # watch that comma at the end.  Try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6
-print end7 + end8 + end9 + end10 + end11 + end12
+print end1 + end2 + end3 + end4 + end5 + end6,  # Do print that line6's outcome + line6's outcome ...
+print end7 + end8 + end9 + end10 + end11 + end12 # Do print that line7's outcome + line8's outcome ...
+# Comma at the end is tha continue  a line and next line
+# IF removing a coomma, Cheese and Burger printed each line
+
 
 
