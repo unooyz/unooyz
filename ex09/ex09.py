829d50eacf5cf890f6dfb1fd11fd8d5f92cc7816
CAD Client <CAD Client>
2016-04-05 11:07:14 +0900

    study drill 09

---
+++ ex09/ex09.py
@@ -0,0 +1,14 @@
+# Here's some new stange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nmay\nJun\nAug" # \ is back slash and \n makes each word  each line.
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+# """ is multi print
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lives if we want, or 5, or 6.
+"""
