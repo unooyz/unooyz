b41713a2a5d631d96ea248c5088cb1ad04a8423c
CAD Client <CAD Client>
2016-04-05 12:18:54 +0900

    Study drill ex11

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -1,5 +1,5 @@
 print "How old are you?",
-age = raw_input()
+age = raw_input() # raw_input accept a Word , So this '25' is not a number, just word "25" , Also it has input () Run's place
 print " How tall are you?",
 height = raw_input()
 print "How much do you weigh?",
@@ -7,3 +7,4 @@ weight = raw_input()
 print "So, you're %r old, %r tall and %r heavy." % (
     age, height, weight
 )
+# Step 1 is Line 1 ~~~
