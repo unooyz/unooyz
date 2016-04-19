48472d95b8c100ee1c60e5ccab2528fc0d303693
CAD Client <CAD Client>
2016-04-19 10:15:06 +0900

    ex14 change %r  to %s  and Check Korean

--- ex14/ex14.py
+++ ex14/ex14.py
@@ -1,7 +1,9 @@
+ #-*-coding:cp949
+# 한글로 입력 방법 Help ->  FInd action -> Search "project encoding" -> Change encoding "X-window-949"
 from sys import argv
 
-script, user_name = argv
-prompt = ">"
+script, user_name = argv # I wrote user_nav and use argv "kyongmook" // 스크랩트 파라만타
+prompt = ">" #this shows " > " of a answer line front
 
 print ("Hi %s, I'm the %s script." % (user_name, script))
 print ("I'd like to ask you a few questions")
@@ -17,6 +19,6 @@ computer = raw_input()
 print ("""
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
-And you have a %r computer. Nice.
+And you have a %s computer. Nice.
 """ % (likes, lives, computer))
 
