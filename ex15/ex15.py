3fe1109459a67f4613fc91c146a2aa4622412957
CAD Client <CAD Client>
2016-04-19 11:06:41 +0900

    ex15_

---
+++ ex15/ex15.py
@@ -0,0 +1,17 @@
+ #-*-coding:cp949
+# http://learnpythonthehardway.org/book/ex15.html
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print ("Here's your file %r:" % filename)
+print (txt.read())
+
+print ("Type the filename again:")
+file_again = raw_input(">")
+
+text_again = open(file_again)
+
+print(text_again.read())
