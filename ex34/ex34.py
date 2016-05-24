4d375d67ad906a45b11a94e9dafe36e300950d92
CAD Client <CAD Client>
2016-05-24 11:33:01 +0900

    ex34 drill and comment

--- ex34/ex34.py
+++ ex34/ex34.py
@@ -2,3 +2,6 @@
 animals = ['bear','tiger','penguin','zebra'] # 'animals 이라는 리스트에는 bear tiger penguin zebar 가 있다
 bear = animals[0] # 그중에 bear는 animal이라는 리스크에서 첫번째이다. 파이썬은 0부터 세기때문에 0이 첫번째이다
 # animals[3] = animals[-1]  : -1은 그 리스트의 마지막과 동잃하다.
+tiger = animals[1]
+penguin = animals[2]
+zebra = [3]
