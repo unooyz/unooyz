f9416153a912e5a6a71b71b680187241f69bac3f
CAD Client <CAD Client>
2016-06-07 10:17:27 +0900

    ex40

---
+++ ex40/ex40.py
@@ -0,0 +1,27 @@
+mystuff = {'apple': "I am apples!"}
+print mystuff['apple']
+
+import mystuff
+mystuff.apple()
+
+def apple():
+    print "Iam apples!"
+
+import mystuff
+
+mystuff.apple()
+print mystuff.tangerine
+
+
+
+mystuff.apple()
+mystuff.tangerine
+mystuff['apple']
+
+class Mystuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print "I am classy apples!"
